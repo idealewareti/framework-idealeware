d5bf3ee6a01b3cf8fe06fe28b91932d9084c5a7d
thiagofmleite <thiagofmleite@gmail.com>
2017-11-07 09:17:46 -0200

    Adicionado Cache-Control

--- src/app/helpers/httpclient.ts
+++ src/app/helpers/httpclient.ts
@@ -2,96 +2,124 @@ import { Injectable } from '@angular/core';
 import { Http, Headers } from '@angular/http';
 import { AppSettings } from "app/app.settings";
 import { Token } from "app/models/customer/token";
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class HttpClient {
 
   private domain: string;
+  headers: Headers;
 
-  constructor(private http: Http) {}
+  constructor(private http: Http) {
+    this.headers = new Headers();
+  }
 
   setUp() {}
-
-  authorize(headers: Headers, token: Token) {
-    headers.append('Authorization', `${token.tokenType} ${token.accessToken}`);
+  
+  /**
+  * Adiciona o Token ao cabeçalho da requisição
+  * @param {Token} token 
+  * @memberof HttpClient
+  */
+  authorize(token: Token) {
+    this.headers.append('Authorization', `${token.tokenType} ${token.accessToken}`);
   }
 
-  setStore(headers: Headers, params = []) {
-    headers.append('Content-Type', 'application/json');
-    headers.append('Access-Control-Allow-Origin', '*');
-    headers.append('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
-    headers.append('Access-Control-Allow-Headers', 'Origin, Content-Type, X-Auth-Token');
-    
+  /**
+  * Adiciona o cabeçalho à requisição
+  * @param {any} [params=[]] 
+  * @param {Token} [token=null] 
+  * @memberof HttpClient
+  */
+  setHeaders(params = [], token: Token = null) {
+    let domain: string = AppSettings.DOMAIN;
     let zipcode = localStorage.getItem('customer_zipcode');
-    headers.append('Domain', AppSettings.DOMAIN);
-    headers.append('storeId', AppSettings.DOMAIN);
-    headers.append('ZipCode', zipcode);
-
+    this.headers = new Headers();
+    this.headers.append('Content-Type', 'application/json');
+    this.headers.append('Access-Control-Allow-Origin', '*');
+    this.headers.append('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
+    this.headers.append('Access-Control-Allow-Headers', 'Origin, Content-Type, X-Auth-Token');
+    this.headers.append('Cache-control', 'no-cache');
+    this.headers.append('Cache-control', 'no-store');
+    this.headers.append('Expires', '0');
+    this.headers.append('Pragma', 'no-cache');
+    this.headers.append('Domain', domain);
+    this.headers.append('storeId', domain);
+    this.headers.append('ZipCode', zipcode);
     params.forEach(param => {
-      headers.append(param['key'], param['value']);
-    })
+      this.headers.append(param['key'], param['value']);
+    });
+    if (token) {
+      this.authorize(token);
+    }
   }
 
-  get(url, token: Token = null, params = []) {
-    let headers = new Headers();
-    this.setStore(headers, params);
-    if (token){
-      this.authorize(headers, token);
-    } 
-      
-    return this.http.get(url, {
-      headers: headers
-    });
+  /**
+   * Executa uma requisição do tipo GET
+   * @param {any} url 
+   * @param {Token} [token=null] 
+   * @param {any} [params=[]] 
+   * @returns {Observable<any>} 
+   * @memberof HttpClient
+   */
+  get(url, token: Token = null, params = []): Observable<any> {
+    this.setHeaders(params);
+    return this.http.get(url, { headers: this.headers  });
   }
 
   getFipe(url) {
-    let headers = new Headers();
-    return this.http.get(url, {
-      headers: headers
-    });
+      return this.http.get(url, {
+        headers: this.headers
+      });
   }
 
-  post(url, data, token: Token = null) {
-    let headers = new Headers();
-    this.setStore(headers);
-    if (token) {
-      this.authorize(headers, token);
-    }
+  /**
+   * Executa uma requisição do tipo POST
+   * @param {any} url 
+   * @param {any} data 
+   * @param {Token} [token=null] 
+   * @returns {Observable<any>} 
+   * @memberof HttpClient
+   */
+  post(url, data, token: Token = null): Observable<any> {
+    this.setHeaders([], token);
     return this.http.post(url, data, {
-      headers: headers
+      headers: this.headers
     });
   }
 
-  put(url, data, token: Token = null) {
-    let headers = new Headers();
-    this.setStore(headers);
-    if (token) {
-      this.authorize(headers, token);
-    }
+  /**
+  * Executa uma requisição do tipo PUT
+  * @param {any} url 
+  * @param {any} data 
+  * @param {Token} [token=null] 
+  * @returns {Observable<any>} 
+  * @memberof HttpClient
+  */
+  put(url, data, token: Token = null): Observable<any> {
+    this.setHeaders([], token);
     return this.http.put(url, data, {
-      headers: headers
+      headers: this.headers
     });
   }
 
-  delete(url, token: Token = null) {
-    let headers = new Headers();
-    this.setStore(headers);
-    if (token) {
-      this.authorize(headers, token);
-    }
+  /**
+   * Executa uma requisição do tipo DELETE
+   * @param {any} url 
+   * @param {Token} [token=null] 
+   * @returns {Observable<any>} 
+   * @memberof HttpClient
+   */
+  delete(url, token: Token = null): Observable<any> {
+    this.setHeaders([], null);
     return this.http.delete(url, {
-      headers: headers
+      headers: this.headers
     });
   }
 
   private handleErrors(res){
-      if(!res.ok) {
-        throw new Error(res.statusText);
-      }
-        
-      else{
-        return res;
-      }      
+      if(!res.ok) throw new Error(res.statusText);
+      else return res;
   }
 
   xhrPost(url: string, toSend){
@@ -102,8 +130,5 @@ export class HttpClient {
       body: JSON.stringify(toSend)
     })
     .then(res => this.handleErrors(res));
-    
   }
-
-
 }
